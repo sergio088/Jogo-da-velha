5b8200f0f7b302287832dd967605a717adf14ec9
sergio <ssergiosantos088@gmail.com>
2025-07-30 19:25:19 -0300

    Acabei

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,16 @@ export default function Home() {
     <div
       className={
         tema
-          ? "bg-gradient-to-br from-blue-500 to-purple-600 min-h-screen"
-          : "bg-gradient-to-br from-yellow-300 to-orange-400 min-h-screen"
+          ? "bg-gradient-to-br from-blue-500 to-purple-600 min-h-screen w-full"
+          : "bg-gradient-to-br from-yellow-300 to-orange-400 min-h-screen w-full"
       }
     >
-      <div className="flex flex-col items-center justify-center h-screen space-y-5">
+      <div className="flex flex-col items-center justify-center h-screen space-y-5 w-full">
         <Content tema={tema} />
         <button
           type="button"
           onClick={() => setTema(!tema)}
-          className="px-6 py-2 rounded-lg shadow-md font-semibold text-white transition-colors duration-300
+          className="px-6 py-2 w-fit rounded-lg shadow-md font-semibold text-white transition-colors duration-300
             bg-purple-600 hover:bg-purple-700
             border-2 border-white
             "
--- src/components/contant.tsx
+++ src/components/contant.tsx
@@ -20,6 +20,8 @@ interface props {
 export default function Content({ tema = false }: props) {
   const [state, setState] = useState<string[]>([]);
   const [turno, setTurno] = useState(true);
+  const [winX, setwinx] = useState(Number);
+  const [winO, setwinO] = useState(Number);
 
   useEffect(() => {
     setState(Array(9).fill(""));
@@ -34,8 +36,16 @@ export default function Content({ tema = false }: props) {
   };
 
   const space = (id: number) => {
-    setTurno(!turno);
+    if (state[id] !== "") return;
+
     setState((prev) => prev.map((p, idx) => (idx === id ? `${vez()}` : p)));
+  };
+
+  const isBoardEmpty = (board: string[]) => board.every((e) => e === "");
+
+  useEffect(() => {
+    if (isBoardEmpty(state)) return;
+
     const combinaçoes = [
       [0, 1, 2],
       [3, 4, 5],
@@ -53,18 +63,47 @@ export default function Content({ tema = false }: props) {
         state[p[1]] === state[p[2]] &&
         state[p[0]] !== ""
       ) {
-        alert(`O ${turno ? "O" : "X"} venceu`);
+        // alert(`O ${turno ? "O" : "X"} venceu`);
         setState(Array(9).fill(""));
+        if (turno) {
+          setwinx(winX + 1);
+          setTurno(true);
+        } else {
+          setwinO(winO + 1);
+          setTurno(true);
+        }
       }
     });
     if (state.every((e) => e !== "")) {
       setState(Array(9).fill(""));
     }
-  };
+    setTurno(!turno);
+  }, [state]);
 
   return (
-    <div>
-      <div className="grid grid-cols-3 gap-2">
+    <div className="flex flex-col lg:flex-row  w-full lg:w-fit lg:space-x-55 gap-10 lg:px-10">
+      <div className="flex flex-col  justify-center items-center lg:w-fit bg-purple-50 border border-black px-20 rounded-2xl m-auto sm:w-1/3 lg:ml-0  ">
+        <h1
+          className={`${
+            tema ? "text-blue-700" : "text-purple-700"
+          }text-3xl font-bold `}
+        >
+          Wins
+        </h1>
+        <div className="flex space-x-5">
+          <div className="flex flex-col">
+            <h1>X</h1>
+            <h2>{winX}</h2>
+          </div>
+
+          <span className="h-20 lg:h-64 w-[1px] inline bg-black"></span>
+          <div className="flex flex-col">
+            <h1>O</h1>
+            <h2>{winO}</h2>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-3 gap-2 ml-auto  w-full sm:w-fit p-10 lg:p-0  ">
         {state.map((item, index) => (
           <div
             onClick={() => space(index)}
@@ -82,6 +121,7 @@ export default function Content({ tema = false }: props) {
           </div>
         ))}
       </div>
+      <div></div>
     </div>
   );
 }
